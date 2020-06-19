31f339e77e6e5e0acfbbc1e6554a8d513ed5fef0
Naman Kumar <namank_11@outlook.com>
2020-06-19 18:31:11 +0530

    Blog Completed withou Users Module

--- blogs/urls.py
+++ blogs/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'blogs'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('titles/', views.titles, name='titles'),
+    path('titles/<int:title_id>/', views.title, name='title'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
-
+from .models import Title
 # Create your views here.
 
 
 def index(request):
     return render(request, 'blogs/index.html')
+
+
+def titles(request):
+    titles = Title.objects.order_by('date_added')
+    context = {'titles': titles}
+    return render(request, 'blogs/titles.html', context)
+
+
+def title(request, title_id):
+    title = Title.objects.get(id=title_id)
+    descriptions = title.description_set.order_by('-date_added')
+    context = {'title': title, 'descriptions': descriptions}
+    return render(request, 'blogs/title.html', context)
